f0d9c539dcc32d5f1ae72c8ff87f64b4b6f5bbfd
LuisSanchez25 <luisanchez25o@gmail.com>
2024-08-06 16:22:07 -0400

    Test kSOM learning function

--- SOM_learn/train.py
+++ SOM_learn/train.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.spatial.distance import cdist
+import math
+import random # Might want to take a closer look at radom number generators in the futuer
 
 class SOM:
     __doc__ = """
@@ -33,10 +36,15 @@ class SOM:
         (The main difference between this paper and our implementation
         is that we also update the immidiate neighbors of the BMU)
         https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=23839
+        ** Could maybe make it more general in the future so it can accept tables**
 
         decay_type (str): The type of decay the SOM should follow.
-        som_type (str): The type of SOM to use. Current options are Kohonen or cSOM, this can be expanded.
+        som_type (str): The type of SOM to use. Current options are Kohonen or cSOM, 
+                        this can be expanded.
         mode (str): The mode of the SOM. Either batch or online.
+
+        learning_parameters['alpha'] (float): The learning rate of the SOM.
+        learning_parameters['sigma'] (float): The initial neighborhood radius of the SOM.
         """
         self.x_dim = x_dim
         self.y_dim = y_dim
@@ -69,23 +77,107 @@ class SOM:
 
         # Decide the order of the input for traiing:
         train_method = self.mode_methods.get(self.mode)
-        if train_method:
-            # Maybe output an array with random indexes to train the SOM?
-            data_shuffled_index = train_method(data, self.n_iter)
+       
+        # Maybe output an array with random indexes to train the SOM?
+        data_shuffled_index = train_method(data, self.n_iter)
 
         # Train the SOM
         if self.som_type == "Kohonen":
+            self.Kohonen_SOM(data, data_shuffled_index)
 
-        for i in range(self.n_iter):
-            for d in data:
-                bmu = self.find_bmu(d)
-                self.update_weights(d, bmu, i)
+        elif self.som_type == "cSOM":
+            self.cSOM(data, data_shuffled_index)   
 
-    def Kohonen_SOM(self, index):
+        else:
+            raise ValueError(f"SOM type {self.som_type} is not supported. Choose from Kohonen or cSOM")
+
+        #return weight_cube
+
+    def Kohonen_SOM(self, data, indecies):
         """
         Train the SOM using the Kohonen algorithm.
         """
+
+        # This might be a bit unnecessary but I will keep it for now.
+        [som_x, som_y, _] = self.weight_cube.shape
+
+        # Might want to pick a mode outside the loop to save time.
+
+        for i in range(self.n_iter):
+            distances = cdist(self.weight_cube.reshape(-1, 
+                                                       self.weight_cube.shape[-1]), 
+                                                       data[indecies[i]], 
+                                                       metric='euclidean')
+
+            w_neuron = np.argmin(distances, axis=0)
+            x_idx, y_idx = np.unravel_index(w_neuron, (self.x_dim, self.y_dim))
+
+            # Need to calculate the sigma radius.
+            # Might want to write this in a more modular way.
+            if self.decay_type == "exponential":
+                sigma = int(self.learning_parameters["sigma"] * np.exp(-i / self.n_iter))
+                radius = math.ceil(self.learning_parameters["max_radius"] * np.exp(-i / self.n_iter))
+            
+            elif self.decay_type == "linear":
+                sigma = int(self.learning_parameters["sigma"] - self.learning_parameters["sigma"] / self.n_iter * i)
+                radius = math.ceil(self.learning_parameters["max_radius"] - self.learning_parameters["max_radius"] / self.n_iter * i)
+           
+            elif self.decay_type == "schedule":
+                # Need to check if the current time step is and pic the sigma from the schedule.
+                current_schedule = np.sum(self.learning_parameters["time"] <= i)
+                sigma = self.learning_parameters["sigma"][current_schedule]
+                radius = self.learning_parameters["max_radius"][current_schedule]  
+
+            else:
+                raise ValueError(f"Decay type {self.decay_type} is not supported. Choose from exponential, linear or schedule")
+
+            # Now compute the neighbors to update
+            x_min, x_max, y_min, y_max = self.compute_neighborhood(x_idx, y_idx, sigma, radius)
+
+            neighborhood_radius = self.neighborhood_function(x_idx, y_idx, radius)
+
+            # This is currently updating the BMU, but we need to update the BMU and its neighbors.
+            # Missing the radius decay. (further away points should be updated less)
+            self.weight_cube[x_min:x_max, y_min:y_max] += self.learning_parameters["alpha"] * neighborhood_radius[x_min:x_max, y_min:y_max] * (data[indecies[i]] - self.weight_cube[x_min:x_max, y_min:y_max])
+        
+    
+    def cSOM(self, index):
+        """
+        Train the SOM using the concious SOM algorithm.
+        """
         raise NotImplementedError
+    
+    def neighborhood_function(self, x_bmu, y_bmu, radius):
+        """
+        Calculate the neighborhood function for the SOM. This function should
+        decay with the distance from the BMU.
+        """
+        x_min, x_max, y_min, y_max = self.compute_neighborhood(x_bmu, y_bmu, radius)
+        update_neighborhood = np.zeros((self.x_dim, self.y_dim))
+
+        # Simple neighborhood function
+        #-=]\update_neighborhood[x_min:x_max, y_min:y_max] = 1 
+
+        # Could maybe speed up with numba
+        for i in range(x_min, x_max):
+            for j in range(y_min, y_max):
+
+                update_neighborhood[i, j] = 1 / 2 ** (np.max((np.abs(x_bmu - i)), np.abs(y_bmu - j) ))#np.exp(-np.linalg.norm([i-x_bmu, j-y_bmu]) / sigma)
+
+        return update_neighborhood
+        # Want to make it so the value is 1 at the BMU and decays with distance.
+
+    
+    def compute_neighborhood(self, x_bmu, y_bmu, sigma, radius):
+        """
+        Compute the neighborhood of the BMU.
+        """
+        x_min = max(0, x_bmu - radius)  
+        x_max = min(self.x_dim, x_bmu + radius)  
+        y_min = max(0, y_bmu - radius)  
+        y_max = min(self.y_dim, y_bmu + radius)
+
+        return x_min, x_max, y_min, y_max
 
     def decay(self):
         """
@@ -102,17 +194,35 @@ class SOM:
         elif self.decay_type == "schedule":
             self
 
-    def _train_batch(self, data, n_iter):
+    def _train_batch(self, data):
         """
         Train the SOM in batch mode.
+        This is making an assumption that the data is bigger than the itteration,
+        this is not always true
         """
-        raise NotImplementedError
+        batches = math.ceil(self.n_iter/ len(data))
+        reminder = self.n_iter % len(data) 
+        indecies = np.zeros(self.n_iter)
+
+        for batch in range(batches):
+            if batch != batches - 1:
+                indecies[batch*len(data): (1 + batch)*len(data)] = random.sample(list(np.arange(len(data))), len(data))
+            else:
+                indecies[(batch)*len(data):] = random.sample(list(np.arange(len(data))), reminder)
+                
+        return indecies
 
     def _train_online(self, data, n_iter):
         """
         Train the SOM in online mode.
         """
-        raise NotImplementedError
+        return random.choices(np.arange(len(data)), k=n_iter) 
+    
+    def weight_cube(self):
+        """
+        Return the weight cube of the SOM.
+        """
+        return self.weight_cube
 
 
 
